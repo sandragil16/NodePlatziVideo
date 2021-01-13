db302e334b552270af4c1c2318e5d5e7559594fe
Sandra Gil <scgmendozaprivate16@gmail.com>
2021-01-12 22:31:06 -0600

    put, status and delete

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -17,5 +17,58 @@ function movieApi(app) {
       next(err);
     }
   });
+
+  
+    router.get('/:movieId' ,async function (req, res, next) {
+      try {
+        const movies = await Promise.resolve(moviesMock[0]);
+  
+        res.status(200).json({
+            data: movies,
+            message: 'movie retrieved'
+        })
+      } catch (err) {
+        next(err);
+      }
+    });
+
+      router.post('/', async function (req, res, next) {
+        try {
+          const createdMovieId = await Promise.resolve(moviesMock[0].id);
+    
+          res.status(201).json({
+              data: createdMovieId,
+              message: 'movies created'
+          })
+        } catch (err) {
+          next(err);
+        }
+      });
+      
+        router.put('/:movieId', async function (req, res, next) {
+          try {
+            const UpdatedMovieId = await Promise.resolve(moviesMock[0].id);
+      
+            res.status(200).json({
+                data: UpdatedMovieId,
+                message: 'movie updated'
+            })
+          } catch (err) {
+            next(err);
+          }
+        });
+        
+          router.delete('/:movieId', async function (req, res, next) {
+            try {
+              const movies = await Promise.resolve([moviesMock[0].id);
+        
+              res.status(200).json({
+                  data: deleteddMovieId,
+                  message: 'movie deleted'
+              })
+            } catch (err) {
+              next(err);
+            }
+          });
 }
 module.exports = moviesApi;
